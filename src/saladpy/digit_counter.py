7bdcb6855c30ec5f543251dcea4853f8750e7d2b
Hlpred <71465959+Hlpred@users.noreply.github.com>
2024-07-05 14:44:36 -0400

    dependencies working

--- src/saladpy/digit_counter.py
+++
@@ -1,8 +0,0 @@
-from decimal import Decimal, getcontext
-
-def count_digits(number, precision):
-    getcontext().prec = precision
-    decimal_number = Decimal(str(number))
-    decimal_places = -decimal_number.as_tuple().exponent
-    
-    return decimal_places
